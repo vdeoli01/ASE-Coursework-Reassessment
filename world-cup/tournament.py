bbe363a47902fd8771895020b4b5e5805a166e4c
Lukene Ngola <visso.ngola@hotmail.com>
2023-08-04 09:02:09 +0000

    Initial Tournament.py implementation

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -14,10 +14,22 @@ def main():
         sys.exit("Usage: python tournament.py FILENAME")
 
     teams = []
-    # TODO: Read teams into memory from file
+    # Read teams into memory from file
+    with open(sys.argv[1], "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            team = {"team": row["team"], "rating": int(row["rating"])}
+            teams.append(team)
 
     counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
+    # Simulate N tournaments and keep track of win counts
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
+    print(counts)
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -46,9 +58,12 @@ def simulate_round(teams):
     return winners
 
 
-def simulate_tournament(teams):
+def simulate_tournament(teams) -> str:
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    return teams[0]["team"]
 
 
 if __name__ == "__main__":
