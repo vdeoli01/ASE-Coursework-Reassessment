d2e4522bf4eb57ee855595b3a04a150f2fb2a4e7
Lukene Ngola <visso.ngola@hotmail.com>
2023-08-06 02:17:05 +0000

    Added test for main

--- world-cup/test-tournament.py
+++ world-cup/test-tournament.py
@@ -1,5 +1,8 @@
-from tournament import simulate_tournament
+from tournament import simulate_tournament, main
 from unittest.mock import patch
+import sys
+from contextlib import redirect_stdout
+import io
 
 
 def test_simulate_tournament():
@@ -14,3 +17,21 @@ def test_simulate_tournament():
     with patch("random.random", return_value=0.5):
         winner = simulate_tournament(teams)
         assert winner == "Team C"  # Highest rating
+
+
+def test_main():
+    filename = "test-teams.csv"
+    # Simulate command-line argument
+    sys.argv = ["tournament.py", filename]
+
+    # Capture output
+    io_stream = io.StringIO()
+    with redirect_stdout(io_stream):
+        # Patching random.random to always return 0.5
+        with patch("random.random", return_value=0.5):
+            main()
+    main_output = io_stream.getvalue()
+    print(main_output)
+
+    assert "Prob of T1 beating T2 is 0.54782" in main_output
+    assert "T1: 100.0% chance of winning" in main_output
